efb1c615e6a2543f745495e397b9f96b96c86bd2
Nathalie Ostrom <nathalie.ostrom@laposte.net>
2015-12-15 11:26:16 +0100

    Update Fourchette.java
    Livraison du code

--- EXOS_JAVA_JEE/Activit-2/Fourchette.java
+++ EXOS_JAVA_JEE/Activit-2/Fourchette.java
@@ -1,1 +1,31 @@
-/*
+import java.util.Scanner;
+import  java.lang.Math;
+public class Fourchette {
+
+	public static void main (String[] arg)  {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entrez x: ");
+		int x = sc.nextInt();
+
+		System.out.print("Entrez y: ");
+		int y = sc.nextInt();
+
+    System.out.print("Entrez z: ");
+		int z = sc.nextInt();
+
+
+		System.out.println("x est il compris entre min(y,z) et max(y,z) ? ");
+		System.out.println("x : " + x);
+		System.out.println("y : " + y);
+    System.out.println("y : " + z);
+
+    System.out.println(" le plus petit = " + Math.min(y, z));
+
+    System.out.println(" le plus grand = " + Math.max(y, z));
+
+    if (Math.min(y, z) < x && x < Math.max(y, z))
+      System.out.println( " réponse x est dans l'intervalle");
+    else
+      System.out.println( " réponse x n'est pas dans l'intervalle");
+			}
+}
